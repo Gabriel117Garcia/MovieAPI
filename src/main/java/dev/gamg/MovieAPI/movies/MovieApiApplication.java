2a5a536836872f354930bd1a7b5bd592b6b11a74
Gabriel Antonio Morales <toni.morales.gmg@gmail.com>
2024-05-11 11:44:54 -0600

    Dockerfile added

--- src/main/java/dev/gamg/MovieAPI/movies/MovieApiApplication.java
+++ src/main/java/dev/gamg/MovieAPI/movies/MovieApiApplication.java
@@ -2,6 +2,9 @@ package dev.gamg.MovieAPI.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,5 +13,11 @@ public class MovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
+
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<String> homePage(){
+		return new ResponseEntity<>("Welcome to the Movie API!", HttpStatus.OK);
 	}
 }
--- src/test/java/dev/gamg/MovieAPI/MovieApiApplicationTests.java
+++ src/test/java/dev/gamg/MovieAPI/MovieApiApplicationTests.java
@@ -1,9 +1,10 @@
 package dev.gamg.MovieAPI;
 
+import dev.gamg.MovieAPI.movies.MovieApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MovieApiApplication.class)
 class MovieApiApplicationTests {
 
 	@Test
